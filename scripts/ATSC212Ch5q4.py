92d722e7334fce25dbb1decf60252e76e9bba6fa
joseph5640 <joseph.palmulli.academic@gmail.com>
2016-03-01 14:46:33 -0800

    Chapter 5 4(a)

---
+++ scripts/ATSC212Ch5q4.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 29 21:25:03 2016
+
+@author: joe
+"""
+
+import numpy as np 
+from matplotlib import pyplot as plt
+import math
+
+#Chapter 5 Question 4(a)
+t,d,dy = np.loadtxt("/Users/joe/Desktop/DataforExercise4b.txt", skiprows=6, unpack=True)
+print(t)
+print(d)
+print(dy)
+
+
+
+
+
+
+
